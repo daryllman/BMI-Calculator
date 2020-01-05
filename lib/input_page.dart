ea8cddb40acdcb91b5806c98e7e449536689cdaa
Daryll Wong <daryll_wong@hotmail.com>
2020-01-06 02:40:41 +0800

    bring out roundicon button

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,11 @@
+import 'package:bmi_calculator/round_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/reusable_card.dart';
 import 'package:bmi_calculator/icon_content.dart';
 import 'constants.dart';
 import 'package:bmi_calculator/results_page.dart';
-import 'bottom_button.dart';
+import 'package:bmi_calculator/bottom_button.dart';
 
 enum Gender {
   male,
@@ -229,24 +230,4 @@ class _InputPageState extends State<InputPage> {
 
 
 
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
 
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      elevation: 0.0,
-    );
-  }
-}
